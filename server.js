5677bcdd1cbb7eb427edcb0fa84ba94666474984
prasa-c <hmc.prasad999@gmail.com>
2025-01-16 00:12:45 +0530

    backend completed

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-import express from 'express'
-import cors from 'cors'
-import { connectDB } from './config/db.js'
-import foodRouter from './routes/foodRoute.js'
-import userRouter from './routes/userRoute.js'
-import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import 'dotenv/config';
+import cardRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 // app config
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // db connection
 connectDB();
 
 // api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
+app.use('/api/food', foodRouter);
+app.use('/images', express.static('uploads'));
+app.use('/api/user', userRouter);
+app.use('/api/cart', cardRouter);
+app.use('/api/order', orderRouter);
 
+app.get('/', (req, res) => {
+  res.send('API Working');
+});
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
-
-app.listen(port,()=>{
-    console.log(`Server Started on http://localhost:${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
